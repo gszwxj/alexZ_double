81666fd92f7f7715affffa5f41d720f36a485c4c
Alex Tan <gszwxj@gmail.com>
2017-08-27 22:45:35 -0500

    commit the test.rb
    stash is complicated, especiall, stash at one branch, then change to another branch and try to apply the stash:    git stash apply

--- test.rb
+++ test.rb
@@ -1,2 +1,3 @@
 test.rb for branch
 add another line to the file after switching back to the master. It has been add one line to this file when it switched to the testing.
+Add a new line to test the stash aug27.
