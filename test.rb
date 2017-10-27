3c22ae180244102104ff287a0e3b2441b31bbf9e
Tianwu(Alex) Tan <tianwu.tan@zayo.com>
2017-10-27 20:28:21 +0000

    git rm test.rb

--- test.rb
+++
@@ -1,3 +0,0 @@
-test.rb for branch
-add another line to the file after switching back to the master. It has been add one line to this file when it switched to the testing.
-Add a new line to test the stash aug27.
